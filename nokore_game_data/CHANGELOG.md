d2f3309cbbad9bbeec8dcda733a91230bef3b66e
Corey Powell <mistdragon100@gmail.com>
2026-08-05 22:17:26 -0500

    nokore_game_data: Updated calling conventions for foundation_binary related modules
    This now uses the V2 MarshallValue for new files, but can still read V1.

--- nokore_game_data/CHANGELOG.md
+++ nokore_game_data/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.10.0
+
+* Updated KVStore to utilize update foundation_binary, marshalling is now done with V2 vs V1, and is NOT backwards compatible once updated, V1 stores can still be read and will continuing being readable for the future, but newly persisted stores will use the V2 format.
+
 # 0.9.0
 
 * Changed `KVStore#increment/2` and `KVStore#decrement/2` return values, they now return the value and self instead of just self
--- nokore_game_data/key_value_store/persist_marshall.lua
+++ nokore_game_data/key_value_store/persist_marshall.lua
@@ -2,161 +2,171 @@ local path_dirname = assert(foundation.com.path_dirname)
 local Buffer = assert(foundation.com.BinaryBuffer or foundation.com.StringBuffer,
                       "expected some kind of buffer")
 
-local KVStore = nokore_game_data.KVStore
-local ic = KVStore.instance_class
-
-local ByteBuf
+local BB_LE
 if foundation.com.ByteBuf then
-  ByteBuf = assert(foundation.com.ByteBuf.little)
+  BB_LE = assert(foundation.com.ByteBuf.LE)
 end
-local MarshallValue
 
+local MarshallValueV1
 if foundation.com.binary_types then
-  MarshallValue = foundation.com.binary_types.MarshallValue
+  MarshallValueV1 = foundation.com.binary_types.MarshallValue.V1
 end
 
-if ByteBuf and MarshallValue then
-  local marshall = MarshallValue:new()
+local MarshallValueV2
+if foundation.com.binary_types then
+  MarshallValueV2 = foundation.com.binary_types.MarshallValue.V2
+end
 
-  function ic:marshall_dump(stream)
-    local bytes_written = 0
-    local bw, err
+local KVStore = nokore_game_data.KVStore
+do
+  local ic = KVStore.instance_class
 
-    -- Write the magic bytes
-    bw, err = ByteBuf:write(stream, "NKKV") -- NoKoreKeyValue
-    bytes_written = bytes_written + bw
-    if err then
+  if BB_LE and MarshallValueV1 or MarshallValueV2 then
+    local marshall_v1 = MarshallValueV1:new()
+    local marshall_v2 = MarshallValueV2:new()
+
+    function ic:marshall_dump(stream)
+      local bytes_written = 0
+      local bw, err
+
+      -- Write the magic bytes
+      bw, err = BB_LE:write(stream, "NKKV") -- NoKoreKeyValue
+      bytes_written = bytes_written + bw
+      if err then
+        return bytes_written, err
+      end
+
+      -- Write the format code
+      bw, err = BB_LE:write(stream, "MRSH") -- MRSH - Marshall, ASCI - ASCII Pack
+      bytes_written = bytes_written + bw
+      if err then
+        return bytes_written, err
+      end
+
+      -- Write the endian code
+      bw, err = BB_LE:write(stream, "LITE") -- LITE - little endian, BIGE - big endian
+      bytes_written = bytes_written + bw
+      if err then
+        return bytes_written, err
+      end
+
+      bw, err = BB_LE:w_i32(stream, 2) -- Version 1
+      bytes_written = bytes_written + bw
+      if err then
+        return bytes_written, err
+      end
+
+      bw, err = marshall_v2:write(BB_LE, stream, self.data) -- marshall dump the data
+      bytes_written = bytes_written + bw
+      if err then
+        return bytes_written, err
+      end
+
+      bw, err = BB_LE:write(stream, "NKEE") -- NoKoreEndEnd
+      bytes_written = bytes_written + bw
+      if err then
+        return bytes_written, err
+      end
+
       return bytes_written, err
     end
 
-    -- Write the format code
-    bw, err = ByteBuf:write(stream, "MRSH") -- MRSH - Marshall, ASCI - ASCII Pack
-    bytes_written = bytes_written + bw
-    if err then
-      return bytes_written, err
+    function ic:marshall_load(stream)
+      local bytes_read = 0
+      local br
+
+      local magic
+      magic, br = BB_LE:read(stream, 4)
+      bytes_read = bytes_read + br
+
+      if magic ~= "NKKV" then
+        error("Cannot reload table, magic bytes do not match (expected:NKKV, got:" .. magic .. ")")
+      end
+
+      local format
+      format, br = BB_LE:read(stream, 4)
+      bytes_read = bytes_read + br
+
+      if format ~= "MRSH" then
+        error("Incorrect format (expected:MRSH, got:".. format .. ")")
+      end
+
+      local byte_order
+      byte_order, br = BB_LE:read(stream, 4)
+      bytes_read = bytes_read + br
+
+      if byte_order ~= "LITE" then
+        error("Unsupported byte order (expected:LITE, got:".. byte_order .. ")")
+      end
+
+      -- little endian encoding
+      local ver
+      ver, br = BB_LE:r_i32(stream)
+      bytes_read = bytes_read + br
+
+      local data
+      if ver == 1 then
+        data, br = marshall_v1:read(BB_LE, stream)
+        bytes_read = bytes_read + br
+      elseif ver == 2 then
+        data, br = marshall_v2:read(BB_LE, stream)
+        bytes_read = bytes_read + br
+      else
+        error("Unsupported version (expected 1 or 2, got ".. ver .. ")")
+      end
+
+      local tail
+      tail, br = BB_LE:read(stream, 4)
+      bytes_read = bytes_read + br
+
+      assert(tail == "NKEE", "expected NKKV stream to end with NKEE")
+
+      self.data = data
+      return self, bytes_read
     end
 
-    -- Write the endian code
-    bw, err = ByteBuf:write(stream, "LITE") -- LITE - little endian, BIGE - big endian
-                                         -- always little because ByteBuf is in little by default
-    bytes_written = bytes_written + bw
-    if err then
-      return bytes_written, err
-    end
+    function ic:marshall_dump_file(filename, trace)
+      --print("marshall_dump_file", filename)
+      local span
+      if trace then
+        span = trace:span_start("mkdir")
+      end
+      core.mkdir(path_dirname(filename))
+      if span then
+        span:span_end()
+      end
 
-    bw, err = ByteBuf:w_i32(stream, 1) -- Version 1
-    bytes_written = bytes_written + bw
-    if err then
-      return bytes_written, err
-    end
+      local buffer = Buffer:new('', 'w')
+      if trace then
+        span = trace:span_start("marshall_dump")
+      end
+      self:marshall_dump(buffer)
+      if span then
+        span:span_end()
+      end
+      buffer:close()
 
-    bw, err = marshall:write(stream, self.data) -- marshall dump the data
-    bytes_written = bytes_written + bw
-    if err then
-      return bytes_written, err
-    end
-
-    bw, err = ByteBuf:write(stream, "NKEE") -- NoKoreEndEnd
-    bytes_written = bytes_written + bw
-    if err then
-      return bytes_written, err
-    end
-
-    return bytes_written, err
-  end
-
-  function ic:marshall_load(stream)
-    local bytes_read = 0
-    local br
-
-    local magic
-    magic, br = ByteBuf:read(stream, 4)
-    bytes_read = bytes_read + br
-
-    if magic ~= "NKKV" then
-      error("Cannot reload table, magic bytes do not match (expected:NKKV, got:" .. magic .. ")")
-    end
-
-    local format
-    format, br = ByteBuf:read(stream, 4)
-    bytes_read = bytes_read + br
-
-    if format ~= "MRSH" then
-      error("Incorrect format (expected:MRSH, got:".. format .. ")")
-    end
-
-    local byte_order
-    byte_order, br = ByteBuf:read(stream, 4)
-    bytes_read = bytes_read + br
-
-    if byte_order ~= "LITE" then
-      error("Unsupported byte order (expected:LITE, got:".. byte_order .. ")")
-    end
-
-    -- little endian encoding
-    local ver
-    ver, br = ByteBuf:r_i32(stream)
-    bytes_read = bytes_read + br
-
-    if ver ~= 1 then
-      error("Unsupported version (expected:1, got:".. ver .. ")")
-    end
-
-    local data
-    data, br = marshall:read(stream)
-    bytes_read = bytes_read + br
-
-    local tail
-    tail, br = ByteBuf:read(stream, 4)
-    bytes_read = bytes_read + br
-
-    assert(tail == "NKEE", "expected NKKV stream to end with NKEE")
-
-    self.data = data
-    return self, bytes_read
-  end
-
-  function ic:marshall_dump_file(filename, trace)
-    --print("marshall_dump_file", filename)
-    local span
-    if trace then
-      span = trace:span_start("mkdir")
-    end
-    minetest.mkdir(path_dirname(filename))
-    if span then
-      span:span_end()
-    end
-
-    local buffer = Buffer:new('', 'w')
-    if trace then
-      span = trace:span_start("marshall_dump")
-    end
-    self:marshall_dump(buffer)
-    if span then
-      span:span_end()
-    end
-    buffer:close()
-
-    if trace then
-      span = trace:span_start("safe_file_write")
-    end
-    minetest.safe_file_write(filename, buffer:blob())
-    if span then
-      span:span_end()
-    end
-    return true
-  end
-
-  function ic:marshall_load_file(filename)
-    --print("marshall_load_file", filename)
-    local f = io.open(filename, 'r')
-    if f then
-      self:marshall_load(f)
-      f:close()
+      if trace then
+        span = trace:span_start("safe_file_write")
+      end
+      core.safe_file_write(filename, buffer:blob())
+      if span then
+        span:span_end()
+      end
       return true
     end
-    return false
+
+    function ic:marshall_load_file(filename)
+      --print("marshall_load_file", filename)
+      local f = io.open(filename, 'r')
+      if f then
+        self:marshall_load(f)
+        f:close()
+        return true
+      end
+      return false
+    end
+  else
+    core.log("warning", "marshall functions are not available for key-value store")
   end
-else
-  minetest.log("warning", "marshall functions are not available for key-value store")
 end
